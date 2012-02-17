84cab0251dcb904f294befc66f2deb66ca901b53
Christopher Foo <chris.foo@gmail.com>
2012-02-16 21:51:56 -0500

    * add copyright notices

--- python3/src/torwuf/web/controllers/sessionbase.py
+++ python3/src/torwuf/web/controllers/sessionbase.py
@@ -1,3 +1,22 @@
+'''Mixins for cookie sessions'''
+#
+#	Copyright (c) 2012 Christopher Foo <chris.foo@gmail.com>
+#
+#	This file is part of Torwuf.
+#
+#	Torwuf is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	Torwuf is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with Torwuf.  If not, see <http://www.gnu.org/licenses/>.
+#
 import base64
 import bson
 import datetime
--- python3/src/torwuf/web/models/authorization.py
+++ python3/src/torwuf/web/models/authorization.py
@@ -1,5 +1,23 @@
-
+'''Authorization database keys'''
+#
+#	Copyright (c) 2012 Christopher Foo <chris.foo@gmail.com>
+#
+#	This file is part of Torwuf.
+#
+#	Torwuf is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	Torwuf is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with Torwuf.  If not, see <http://www.gnu.org/licenses/>.
+#
 class TokenCollection(object):
 	COLLECTION = 'authorization_tokens'
-	KEY = 'key'
-	GROUP = 'group'
+#	KEY = 'key'
+#	GROUP = 'group'
