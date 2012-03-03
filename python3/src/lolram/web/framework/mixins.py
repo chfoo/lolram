dbbe030c306def7cd822d079c3d88851adcebcd7
Christopher Foo <chris.foo@gmail.com>
2012-03-03 00:37:09 -0500

    * Add file download mixin

---
+++ python3/src/lolram/web/framework/mixins.py
@@ -0,0 +1,110 @@
+'''Handler mixins'''
+#
+#	Copyright © 2011-2012 Christopher Foo <chris.foo@gmail.com>
+#
+#	This file is part of Lolram.
+#
+#	Lolram is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	Lolram is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with Lolram.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Copyright 2009 Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#	 http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+from tornado.web import HTTPError
+import datetime
+import email.utils
+import hashlib
+import mimetypes
+import os.path
+import stat
+import time
+
+class StaticFileMixIn(object):
+	def serve_file(self, file_path, filename=None, include_body=True):
+		abspath = file_path
+		
+		if not os.path.exists(abspath):
+			raise HTTPError(404)
+		if not os.path.isfile(abspath):
+			raise HTTPError(403, "%s is not a file", file_path)
+
+		stat_result = os.stat(abspath)
+		modified = datetime.datetime.fromtimestamp(stat_result[stat.ST_MTIME])
+
+		self.set_header("Last-Modified", modified)
+
+		if filename:
+			mime_type, encoding = mimetypes.guess_type(filename)
+		else:
+			mime_type, encoding = mimetypes.guess_type(abspath)
+		
+		if mime_type:
+			self.set_header("Content-Type", mime_type)
+
+		cache_time = self.get_cache_time(file_path, modified, mime_type)
+
+		if cache_time > 0:
+			self.set_header("Expires", datetime.datetime.utcnow() + \
+									   datetime.timedelta(seconds=cache_time))
+			self.set_header("Cache-Control", "max-age=" + str(cache_time))
+		else:
+			self.set_header("Cache-Control", "public")
+		
+		if filename:
+			self.set_header('Content-Disposition', 'attachment; filename={}'.format(filename))
+
+		# Check the If-Modified-Since, and don't send the result if the
+		# content has not been modified
+		ims_value = self.request.headers.get("If-Modified-Since")
+		if ims_value is not None:
+			date_tuple = email.utils.parsedate(ims_value)
+			if_since = datetime.datetime.fromtimestamp(time.mktime(date_tuple))
+			if if_since >= modified:
+				self.set_status(304)
+				return
+
+		with open(abspath, "rb") as file:
+			data = file.read()
+			hasher = hashlib.sha1()
+			hasher.update(data)
+			self.set_header("Etag", '"%s"' % hasher.hexdigest())
+			if include_body:
+				self.write(data)
+			else:
+				assert self.request.method == "HEAD"
+				self.set_header("Content-Length", len(data))
+		
+		self.finish()
+
+	def get_cache_time(self, path, modified, mime_type):
+		"""Override to customize cache control behavior.
+
+		Return a positive number of seconds to trigger aggressive caching or 0
+		to mark resource as cacheable, only.
+
+		By default returns cache expiry of 10 years for resources requested
+		with "v" argument.
+		"""
+		return self.CACHE_MAX_AGE if "v" in self.request.arguments else 0
