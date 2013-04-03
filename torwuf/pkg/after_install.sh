9b32949e70678afe138f87295b574129f069802f
Christopher Foo <chris.foo@gmail.com>
2013-04-03 12:35:17 -0400

    Ensure deb package data is owned by root.

--- torwuf/pkg/after_install.sh
+++ torwuf/pkg/after_install.sh
@@ -8,7 +8,6 @@ sudo -u www-torwuf touch /home/www-torwuf/torwuf-service/www/dummy.fcgi
 sudo -u www-torwuf mkdir -p /home/www-torwuf/torwuf-service/www/z/
 sudo -u www-torwuf ln -f -s /opt/torwuf/share/www/favicon.ico /home/www-torwuf/torwuf-service/www/favicon.ico
 chmod +x /opt/torwuf/sbin/*
-chown root:root /etc/cron.d/torwuf-service
 chmod 644 /etc/cron.d/torwuf-service
 
 start torwuf-service
